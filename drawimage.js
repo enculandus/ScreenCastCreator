d490ea5df0c185e8aaafa72f5256adf4f3f7955c
Aniket Nadkarni <aniket120301@gmail.com>
2020-08-18 15:53:57 +0530

    Image drawing improved, resizing allowed

--- drawimage.js
+++ drawimage.js
@@ -7,10 +7,15 @@
 		var filechooser  = document.getElementById("furl");
 
 
-		function previewImage(input){
+	async	function previewImage(input){
 			// this function is to show preview of image
 			var files = event.target.files;
 			var file = files[0];
+			toggle_sidepanel();
+			document.getElementById('image_input_menu').style.margin = "7.5% 0% 0% 15%";
+			document.getElementById('image_input_menu').style.height = "70%";
+			document.getElementById('image_input_menu').style.width = "70%";
+			document.getElementById('image_input_menu').style.visibility = "visible";
 
 			if(file.type !== '' && !file.type.match('image.*'))
         {
@@ -25,9 +30,25 @@
 			};
 			reader.readAsDataURL(input.files[0]);
 		 }
+		 document.getElementById("input_image_width").addEventListener("input", resize_input_image ,{passive: true});
+		 document.getElementById("input_image_height").addEventListener("input", resize_input_image ,{passive: true});
 		}
 
-		function imgavailable(){
+async function resize_input_image() {
+	document.getElementById("preview").style.height = document.getElementById("input_image_height").value + "px";
+	document.getElementById("preview").style.width = document.getElementById("input_image_width").value + "px";
+}
+
+	async	function imgavailable(){
+
+			document.getElementById('image_input_menu').style.margin = "0px 0px 0px 0px";
+			document.getElementById('image_input_menu').style.height = "0%";
+			document.getElementById('image_input_menu').style.width = "0%";
+			document.getElementById('image_input_menu').style.visibility = "hidden";
+			document.getElementById('image_placement_box').style.visibility = "visible";
+			document.getElementById('image_placement_box').style.height = "50px";
+			document.getElementById('image_placement_box').style.width = "90px";
+			document.getElementById('image_placement_box').style.margin = "80% 0% 0% 0%";
 
 			if(filechooser.value != ""){
 				initi3();
@@ -38,7 +59,7 @@
 
 		}
 
-		function setImage(x, y){
+async function setImage(x, y){
 
 	//		var image = document.getElementById("preview");
 			if(!image.complete){
@@ -47,14 +68,18 @@
 				},50);
 				return;
 			}
-
-			context.drawImage(image, x, y);
+			if(canv.width>canv.height){
+				context.font = "30px Arial";
+				context.fillStyle = "white";
+				context.fillText("Tip:Drag The image to its desired location", 86, canv.height-25);
+				context.drawImage(image, (x - (document.getElementById("input_image_width").value/2)), (y - (document.getElementById("input_image_height").value/2)), document.getElementById("input_image_width").value, document.getElementById("input_image_height").value);
+			}
 		}
 
-function initi3() {
+async function initi3() {
   //Setting up for normal moving
 	showCanvas();
-	setImage(0, 0);
+	setImage(document.getElementById("input_image_width").value/2, document.getElementById("input_image_height").value/2);
 	canvas2.addEventListener("touchstart", start_move);
 	canvas2.addEventListener("touchend", stop_move);
 	canvas2.addEventListener("touchmove", move);
@@ -68,7 +93,7 @@ function initi3() {
 //var loca ={x:0 , y:0};
 var movement = false;
 
-function showCanvas() {
+async function showCanvas() {
 				if(audiostream){
 					recorder.pauseRecording();
 				}
@@ -83,13 +108,17 @@ function showCanvas() {
 
         }
 
-function hideCanvas() {
+async function hideCanvas() {
 	canvas2.style.visibility = 'hidden';
 	canvas2.width = '0';
 	canvas2.height = '0';
 	if(audiostream){
 		recorder.resumeRecording();
 	}
+	document.getElementById('image_placement_box').style.margin = "0px 0px 0px 0px";
+	document.getElementById('image_placement_box').style.height = "0%";
+	document.getElementById('image_placement_box').style.width = "0%";
+	document.getElementById('image_placement_box').style.visibility = "hidden";
 }
 
 async function start_move(event) {
@@ -114,24 +143,28 @@ async function move(event){
   setImage(x, y);
 }
 
-function clearpage() {
+async function clearpage() {
   context.clearRect(0,0,canvas2.width,canvas2.height);
-  context.fillStyle = document.getElementById("boardcolor").value;
-  context.fillRect(0, 0, canvas2.width, canvas2.height);
+  //context.fillStyle = document.getElementById("boardcolor").value;
+  //context.fillRect(0, 0, canvas2.width, canvas2.height);
 }
 
-function saveimgdata(){
+async function saveimgdata(){
 	if(filechooser.value != ""){
 				copy();
 			}
 			else{
 				alert("First select Image to upload!");
 			}
+			document.getElementById('image_placement_box').style.margin = "0px 0px 0px 0px";
+			document.getElementById('image_placement_box').style.height = "0%";
+			document.getElementById('image_placement_box').style.width = "0%";
+			document.getElementById('image_placement_box').style.visibility = "hidden";
 }
 
-function copy(){
-	var imgdata = context.getImageData(loc.x, loc.y, image.width, image.height);
-	cntx.putImageData(imgdata, loc.x, loc.y);
+async function copy(){
+	var imgdata = context.getImageData((loc.x-(document.getElementById("input_image_width").value/2)), (loc.y-(document.getElementById("input_image_height").value/2)), document.getElementById("input_image_width").value, document.getElementById("input_image_height").value);
+	cntx.putImageData(imgdata, (loc.x-(document.getElementById("input_image_width").value/2)), (loc.y-(document.getElementById("input_image_height").value/2)));
 	clearpage();
 	hideCanvas();
 
